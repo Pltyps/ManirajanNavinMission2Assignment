150d630c5b4613bae15cf3059ca1bf7f1993a64f
Pltyps <nm356@byu.edu>
2025-01-15 16:56:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        int DiceCount = 0;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        
+        DiceCount = int.Parse(System.Console.ReadLine());
+        
+        // Testing purposes
+        System.Console.WriteLine("Rolls Requested: " + DiceCount);
+    }
+
+    private static void Rolls(int[] arr)
+    {
+        for ()
+    }
+}
